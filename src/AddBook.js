57dbc5c0800c6fe35cfd4448d043283ee5c592fc
Sanjib Ahmad <sanjib@sanjib.org>
2019-03-18 21:02:15 +0600

    Create components and refactor code

---
+++ src/AddBook.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class AddBook extends Component {
+  render() {
+    return "";
+  }
+}
+
+export default AddBook;
---
+++ src/BookDetails.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class BookDetails extends Component {
+  render() {
+    return "";
+  }
+}
+
+export default BookDetails;
